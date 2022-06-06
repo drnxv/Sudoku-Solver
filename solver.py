249e48f2f7cff88d4632ff5ddbe31653c35f23d1
drnxv <87783549+drnxv@users.noreply.github.com>
2022-06-05 20:17:12 -0400

    added comments

--- solver.py
+++ solver.py
@@ -12,16 +12,22 @@ board = [
 ]
 
 def solve(board):
+
+  # finds the empty spot
   empty = empty_pos(board)
+  # board is full
   if not empty:
     return True
   else:
+    # unpack tuple to get row and col values
     row, col = empty
 
+  # for loop to test each number from 1-9
   for i in range(1, 10):
     if valid_number(board, i, (row, col)):
       board[row][col] = i
 
+      # recurse so we test all numbers while backtracking as well
       if solve(board):
         return True
 
@@ -29,19 +35,22 @@ def solve(board):
       board[row][col] = 0
 
 def valid_number(board, num, pos):
-  # check the row
+  
+  # check the row if the number can work
   for i in range(len(board[0])):
     if board[pos[0]][i] == num and pos[1] != i:
       return False
 
-  # check the column
+  # check the column if the number can work
   for i in range(len(board)):
     if board[i][pos[1]] == num and pos[0] != i:
       return False
 
+  # find which 3x3 grid we need to go to
   box_x = pos[1] // 3
   box_y = pos[0] // 3
 
+  # checking if the num there can't work
   for i in range(box_y * 3, box_y + 3):
     for j in range(box_x * 3, box_x + 3):
       if board[i][j] == num and (i, j) != pos:
@@ -49,6 +58,7 @@ def valid_number(board, num, pos):
 
   return True
 
+# prints the board in a sudoku board looking fashion
 def print_board(board):
   for i in range(len(board)):
     if i % 3 == 0 and i != 0:
@@ -63,15 +73,16 @@ def print_board(board):
       else:
         print(str(board[i][j]) + " ", end='')
 
+# finds an empty position and returns that as a tuple (row, col)
 def empty_pos(board):
   for i in range(len(board)):
     for j in range(len(board[i])):
       if board[i][j] == 0:
-        # returns (row, col)
         return (i, j)
 
   return None
 
+# main function calling all functions
 def main():
   print_board(board)
   print('=' * 50)
@@ -79,6 +90,7 @@ def main():
   print_board(board)
   print('Solved')
 
+# run main
 if __name__ == '__main__':
   main()
   
